720b9faedd44a7c5513528fc44939d2a3aebe50c
dyna-mis <guangping@ac.tuwien.ac.at>
2022-02-28 20:42:05 +0100

    linux adaption 6

--- E4-Rect/scripts/ALLinALL.py
+++ E4-Rect/scripts/ALLinALL.py
@@ -19,7 +19,7 @@ instance_list = ["post_office_box",
 
 for mod in ['add', 'sub']:
   for model in ['uniform', 'gaussian']:
-            dirname = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
+            dirname = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))))
             result_path = Path(os.path.join(dirname, "dynamis_jea", "OUTPUT", "E4_Rect"))
             assert result_path.is_dir()
             outPutName = os.path.join(os.path.dirname(os.path.dirname(__file__)), mod+'-'+model+'-'+'rectangle')
