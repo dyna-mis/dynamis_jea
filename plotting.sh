3e89198abcfc5b155593821180d16b647ff3f561
Guangping Li <ailixiaod@gmail.com>
2022-02-28 15:40:00 +0100

    linux adaption

--- plotting.sh
+++ plotting.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-declare -a exps =("E1" "E1_Rect" "E2" "E2_Rect" "E3" "E3_Rect" "E4" "E4_Rect" "E5" "E5_Rect")
+#declare -a exps=("E1" "E1_Rect" "E2" "E2_Rect" "E3" "E3_Rect" "E4" "E4_Rect" "E5" "E5_Rect")
+declare -a exps=("E1")
 #E1* 10000 400
 #  E1
-for exp in ${exp[@]};
+for exp in "${exps[@]}";
 	do	
-		$exp/ALLinALL.py 
+		python3 $exp/scripts/ALLinALL.py 
 	done
 
 
