aba8cce303e51a846822dd6a586febdcefce3ee8
dyna-mis <guangping@ac.tuwien.ac.at>
2022-02-28 15:45:41 +0100

    *

--- E1/scripts/ALLinALL.py
+++ E1/scripts/ALLinALL.py
@@ -10,9 +10,11 @@ for mod in ['add','sub']:
 
 for mod in ['add', 'sub']:
   for model in ['uniform', 'gaussian']:
-        result_path = Path('../../../dynamis_jea/OUTPUT/E1')
+        dirname = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))))
+        result_path = Path(os.path.join(dirname, "dynamis_jea", "OUTPUT", "E1"))
         print(result_path)
         assert result_path.is_dir()
         outPutName = os.path.dirname(os.path.dirname(__file__)) + '\plots'+"\\"+mod+'-'+model
+        outPutName = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'plots', mod+'-'+model)
 
         scatterPlot.getScatter_UPDATE_SIZE(result_path, mod, model,'no',outPutName)
--- E2/scripts/ALLinALL.py
+++ E2/scripts/ALLinALL.py
@@ -24,7 +24,7 @@ for instance in instance_list:
             result_path = Path(os.path.join(dirname, "dynamis_jea", "OUTPUT", "E23")) 
             print(result_path)
             assert result_path.is_dir()
-            outPutName = os.path.dirname(os.path.dirname(__file__)) + '\plots' + "\\"+mod+'-'+instance+'-'+'square'
+            outPutName = Path(os.path.join(os.path.dirname(os.path.dirname(__file__)) ,'plots' ,mod,'-',instance, '-','square'))
             scatterPlot.getScatter_UPDATE_RATIO(result_path, instance, mod,'no',outPutName)
 
 
