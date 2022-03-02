ad357e7d60189225976f34e4380af66e04e5e16b
dyna-mis <guangping@ac.tuwien.ac.at>
2022-03-02 08:57:42 +0100

    *

--- plotting.sh
+++ plotting.sh
@@ -1,8 +1,6 @@
 #!/bin/bash 
 
 declare -a exps=("E1" "E1-Rect" "E2" "E2-Rect" "E3" "E3-Rect" "E4" "E4-Rect" "E5" "E5-Rect")
-#E1* 10000 400
-#  E1
 for exp in "${exps[@]}";
 	do	
 		python3 $exp/scripts/ALLinALL.py 
